152371a5ed36576ecaa504831c0531117a72bde9
Bence Tuzson <tuzsonbence2003@gmail.com>
2020-09-27 01:58:24 +0200

    Final product (I hope)

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,65 @@
 package main;
+
 import main.model.Fraction;
+
+import java.io.*;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Fraction f1 = new Fraction(8, 4);
-        System.out.println(f1);
-        System.out.println(f1.getExpandedForm(3));
-        System.out.println(f1.getSimplestForm());
-        System.out.println(f1.getExpandedForm(3).getSimplestForm());
+        final String path = "src/resources/input.txt";
+        final String path2 = "src/resources/output.txt";
+        String next;
+
+        try {
+            Scanner sc = new Scanner(new BufferedReader(new FileReader(path)));
+            Fraction fraction = new Fraction();
+            sc.useDelimiter("\n");
+            while (sc.hasNext()) {
+                next = sc.next();
+                next = next.replace(String.valueOf(next.charAt(next.length()-1)), "");
+
+                switch (next.split(" ").length) {
+                    case 1:
+                        fraction = new Fraction(Integer.parseInt(next.split("/")[0]), Integer.parseInt(next.split("/")[1]));
+                        break;
+                    case 2:
+                        switch (next.split(" ")[0]) {
+                            case "+":
+                                switch (next.split(" ")[1].split("/").length) {
+                                    case 2 -> fraction.add(new Fraction(Integer.parseInt(next.split(" ")[1].split("/")[0]), Integer.parseInt(next.split(" ")[1].split("/")[1])));
+                                    case 1 -> fraction.add(Objects.requireNonNull(Fraction.parseString(next.split(" ")[1])));
+                                }
+                                break;
+                            case "-":
+                                switch (next.split(" ")[1].split("/").length) {
+                                    case 2 -> fraction.sub(new Fraction(Integer.parseInt(next.split(" ")[1].split("/")[0]), Integer.parseInt(next.split(" ")[1].split("/")[1])));
+                                    case 1 -> fraction.sub(Objects.requireNonNull(Fraction.parseString(next.split(" ")[1])));
+                                }
+                                break;
+                            case "*":
+                                switch (next.split(" ")[1].split("/").length) {
+                                    case 2 -> fraction.mul(new Fraction(Integer.parseInt(next.split(" ")[1].split("/")[0]), Integer.parseInt(next.split(" ")[1].split("/")[1])));
+                                    case 1 -> fraction.mul(Objects.requireNonNull(Fraction.parseString(next.split(" ")[1])));
+                                }
+                                break;
+                            case "/":
+                                switch (next.split(" ")[1].split("/").length) {
+                                    case 2 -> fraction.div(new Fraction(Integer.parseInt(next.split(" ")[1].split("/")[0]), Integer.parseInt(next.split(" ")[1].split("/")[1])));
+                                    case 1 -> fraction.div(Objects.requireNonNull(Fraction.parseString(next.split(" ")[1])));
+                                }
+                                break;
+                        }
+                }
+            }
+            try (FileWriter fw = new FileWriter(path2)) {
+                fw.write(fraction.toString() + "\n" + (double) fraction.getNumerator()/fraction.getDenominator());
+            } catch (IOException e) {
+                System.err.println("Error while writing into file!");
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found!");
+        }
     }
 }
--- src/main/model/Fraction.java
+++ src/main/model/Fraction.java
@@ -2,20 +2,20 @@ package main.model;
 
 import java.util.Objects;
 
-import static main.model.MathUtil.GCD;
+import static main.model.MathUtil.*;
 
-
-public class Fraction {
+@SuppressWarnings("ConstantConditions")
+public class Fraction implements Comparable<Fraction> {
     //2-tes szint
     private int numerator;
     private int denominator;
     private boolean isInteger = false;
-    final private static int pi = 355/113;
+    final private static Fraction pi = new Fraction(355, 113);
 
     public Fraction(int numerator, int denominator) {
         this.numerator = numerator;
         this.denominator = denominator;
-        if ((numerator / denominator) % 2 == 0) {
+        if (this.numerator % this.denominator == 0) {
             isInteger = true;
         }
         this.simplify();
@@ -36,70 +36,77 @@ public class Fraction {
     public Fraction(int numerator, int denominator, boolean simplify) {
         this.numerator = numerator;
         this.denominator = denominator;
-        if ((numerator / denominator) % 2 == 0) {
-            isInteger = true;
-        }
         if (simplify) {
             this.simplify();
+        } else {
+            isInteger = this.numerator % this.denominator == 0;
         }
     }
 
     public Fraction(Fraction fraction, boolean simplify) {
-        this.numerator = fraction.numerator;
-        this.denominator = fraction.denominator;
+        numerator = fraction.numerator;
+        denominator = fraction.denominator;
         if (simplify) {
             this.simplify();
+        } else {
+            isInteger = this.numerator % this.denominator == 0;
         }
     }
 
     public Fraction(Fraction fraction) {
         numerator = fraction.numerator;
         denominator = fraction.denominator;
+        isInteger = fraction.isInteger;
     }
 
     public Fraction add(Fraction fraction) {
-        this.numerator += fraction.numerator;
-        this.denominator += fraction.denominator;
+        Fraction ret = new Fraction();
+        ret.denominator = LCM(denominator, ret.denominator, fraction.denominator);
+        setNumerator(ret.numerator += ret.denominator / fraction.denominator * fraction.numerator + ret.denominator / denominator * numerator);
+        setDenominator(ret.denominator);
+        this.simplify();
         return this;
     }
 
     public Fraction sub(Fraction fraction) {
-        this.numerator -= fraction.numerator;
-        this.denominator -= fraction.denominator;
+        Fraction ret = new Fraction();
+        ret.denominator = LCM(denominator, ret.denominator, fraction.denominator);
+        setNumerator(ret.numerator -= ret.denominator / fraction.denominator * fraction.numerator - ret.denominator / denominator * numerator);
+        setDenominator(ret.denominator);
+        this.simplify();
         return this;
     }
 
     public Fraction mul(Fraction fraction) {
-        this.numerator *= fraction.numerator;
-        this.denominator *= fraction.denominator;
+        numerator *= fraction.numerator;
+        denominator *= fraction.denominator;
+        this.simplify();
         return this;
     }
 
     public Fraction div(Fraction fraction) {
-        this.numerator /= fraction.numerator;
-        this.denominator /= fraction.denominator;
+        numerator *= fraction.denominator;
+        denominator *= fraction.numerator;
+        this.simplify();
         return this;
     }
 
     public Fraction pow(int power) {
-        this.numerator = (int) Math.pow(this.numerator, power);
-        this.denominator = (int) Math.pow(this.denominator, power);
+        setNumerator((int) Math.pow(numerator, power));
+        setDenominator((int) Math.pow(denominator, power));
+        this.simplify();
         return this;
     }
 
     public Fraction opposite() {
-        this.numerator = -numerator;
-        return this;
+        return new Fraction(-numerator, denominator).getSimplestForm();
     }
 
     public Fraction reciprocal() {
         try {
-            int temp = this.numerator;
-            this.numerator = this.denominator;
-            this.denominator = temp;
-            return this;
+            return new Fraction(denominator, numerator).getSimplestForm();
         } catch (ArithmeticException e) {
-            System.err.println("A tört reciproka nem kétezik");
+            System.err.println("The fraction's reciprocal doesn't exist!");
             return null;
         }
     }
@@ -112,7 +119,7 @@ public class Fraction {
         try {
             this.denominator = denominator;
         } catch (IllegalArgumentException e) {
-            System.err.println("Hibás nevező");
+            System.err.println("Illegal denominator!");
         }
     }
 
@@ -125,81 +132,140 @@ public class Fraction {
     }
 
     public boolean equals(Fraction fraction) {
-        this.simplify();
-        if (fraction == this) {
+        if (fraction.getSimplestForm().numerator == getSimplestForm().numerator && fraction.getSimplestForm().denominator == getSimplestForm().denominator) {
             return true;
         } else if (fraction == null) {
             return false;
         } else {
-            //Fraction f = (Fraction) fraction;
             return false;
         }
     }
 
     public int hashCode() {
-        return Objects.hash(this.numerator, this.denominator);
+        return Objects.hash(numerator, denominator);
     }
-
 
     @Override
     public String toString() {
-        return this.numerator + "/" + this.denominator;
+        if (isInteger) {
+            return Integer.toString((int) evaluate());
+        } else {
+            return numerator + "/" + denominator;
+        }
     }
 
     //3-mas szint
 
     private void simplify() {
-        int GCD = GCD(this.numerator, this.denominator);
+        int GCD = GCD(numerator, denominator);
         denominator /= GCD;
         numerator /= GCD;
+        isInteger = numerator % denominator == 0;
     }
 
-    private void expand(int i) {
-        numerator += denominator*i;
+    private void expand(int n) {
+        numerator += denominator*n;
+        isInteger = numerator % denominator == 0;
     }
 
     public Fraction getSimplestForm() {
-        this.simplify();
-        return this;
+        Fraction ret = new Fraction(numerator, denominator);
+        ret.simplify();
+        return ret;
     }
 
-    public Fraction getExpandedForm(int i) {
-        this.expand(i);
-        return this;
+    public Fraction getExpandedForm(int n) {
+        Fraction ret = new Fraction(numerator, denominator);
+        ret.expand(n);
+        return ret;
     }
 
     //4-es szint
 
-    public Fraction add(Fraction newFraction, Fraction fraction) {
-        newFraction.numerator += fraction.numerator;
-        newFraction.denominator += fraction.denominator;
-        return newFraction;
+    public static Fraction add(Fraction ...fraction) {
+        Fraction ret = new Fraction();
+        for (Fraction num : fraction) {
+            ret.denominator = LCM(num.denominator, ret.denominator);
+        }
+        for (Fraction num : fraction) {
+            ret.numerator += ret.denominator / num.denominator * num.numerator;
+        }
+        return ret.getSimplestForm();
     }
 
-    public Fraction sub(Fraction newFraction, Fraction fraction) {
-        newFraction.numerator -= fraction.numerator;
-        newFraction.denominator -= fraction.denominator;
-        return newFraction;
+    public static Fraction sub(Fraction ...fraction) {
+        Fraction ret = new Fraction();
+        for (Fraction num : fraction) {
+            ret.denominator = LCM(num.denominator, ret.denominator);
+        }
+        fraction[0] = fraction[0].opposite();
+        for (Fraction num : fraction) {
+            ret.numerator -= ret.denominator / num.denominator * num.numerator;
+        }
+        return ret.getSimplestForm();
     }
 
-    public Fraction mul(Fraction newFraction, Fraction fraction) {
-        newFraction.numerator *= fraction.numerator;
-        newFraction.denominator *= fraction.denominator;
-        return newFraction;
+    public static Fraction mul(Fraction ...fraction) {
+        Fraction ret = new Fraction(1, 1);
+        for (Fraction num : fraction) {
+            ret.numerator *= num.numerator;
+            ret.denominator *= num.denominator;
+        }
+        return ret.getSimplestForm();
     }
 
-    public Fraction div(Fraction newFraction, Fraction fraction) {
-        newFraction.numerator /= fraction.numerator;
-        newFraction.denominator /= fraction.denominator;
-        return newFraction;
+    public static Fraction div(Fraction ...fraction) {
+        Fraction ret = new Fraction(1, 1);
+        fraction[0] = fraction[0].reciprocal();
+        for (Fraction num : fraction) {
+            ret.numerator *= num.denominator;
+            ret.denominator *= num.numerator;
+        }
+        return ret.getSimplestForm();
     }
 
     public Fraction pow(Fraction fraction, int power) {
-        fraction.numerator = (int) Math.pow(this.numerator, power);
-        fraction.denominator = (int) Math.pow(this.denominator, power);
-        return fraction;
+        fraction.numerator = (int) Math.pow(numerator, power);
+        fraction.denominator = (int) Math.pow(denominator, power);
+        return fraction.getSimplestForm();
     }
 
     //5-ös szint
-}
 
+    public double evaluate() {
+        return (double) numerator/denominator;
+    }
+
+    public static Fraction parseDouble(Double raw) {
+        if (raw == Math.floor(raw)) {
+            return new Fraction((int) Math.floor(raw), 1).getSimplestForm();
+        } else if (raw == Math.PI){
+            return pi;
+        } else {
+            final int numLength = (int) Math.pow(10, raw.toString().split("\\.")[1].length());
+            return new Fraction((int) (raw*numLength), numLength).getSimplestForm();
+        }
+    }
+
+    public static Fraction parseString(String raw) {
+        try {
+            return parseDouble(Double.parseDouble(raw));
+        } catch (IllegalArgumentException e) {
+            System.err.println("String couldn't be converted into fraction!");
+            return null;
+        }
+    }
+
+    @Override
+    public int compareTo(Fraction o) {
+        if (o.equals(this)) {
+            return 0;
+        } else if (evaluate() < (double) (o.numerator/o.denominator)) {
+            return -1;
+        } else if (evaluate() > (double) (o.numerator/o.denominator)) {
+            return 1;
+        } else {
+            return evaluate() < (double) (o.numerator/o.denominator) ? -1 : 1;
+        }
+    }
+}
